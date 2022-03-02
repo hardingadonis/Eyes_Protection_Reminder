9b87bdb0fb0600af79e41d1ba79fc774c38d635f
Harding Adonis <hardingadonis@gmail.com>
2022-03-02 21:23:28 +0700

    udpdate readme and license

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2022 Harding Adonis, AlexPhoenix45
+Copyright (c) 2022 Harding Adonis, hoanghy0112, AlexPhoenix45
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
