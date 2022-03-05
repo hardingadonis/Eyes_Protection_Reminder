5f8bc9eab6abd489176bc2293975d07a1ef440c1
Hoang Hy <hoanghy01122003@gmail.com>
2022-03-05 21:16:06 +0700

    Remove landing project, preserve build file

--- src/ui/landing.py
+++ src/ui/landing.py
@@ -1,14 +1,10 @@
 import eel
 import sys
 
-eel.init('landing/dist')
+eel.init('landing')
 
 @eel.expose
 def log_eel():
    print(eel)
-
-@eel.expose
-def exit():
-   sys.exit()
 
 eel.start('index.html', size=(500, 800))
--- src/ui/landing/src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-import logo from '../assets/Logo.svg'
-
-export default function Home() {
-   return <div className='grid place-items-center gap-7 mt-16'>
-      <img src={logo} alt="" />
-      <h1 className='font-bold text-5xl'>Eyes Protection Reminder</h1>
-   </div>
-}
