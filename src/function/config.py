993fd611d62ddaafbc85fc99503047a2a6ababc6
Hoang Hy <hoanghy01122003@gmail.com>
2022-03-06 17:29:08 +0700

    Change config.py

--- src/function/config.py
+++ src/function/config.py
@@ -1,16 +1,16 @@
 
 from utils.stateFactory import stateFactory
 
-# Every <timing> minutes, the app will notify user to relax
-get_timing, set_timing = stateFactory(2, 'timing')
+# Every <timing> seconds, the app will notify user to relax
+get_timing, set_timing = stateFactory(20, 'timing')
 
-# duration of the notification
-get_duration, set_duration = stateFactory(5, 'duration')
+# duration of the notification on second
+get_duration, set_duration = stateFactory(10, 'duration')
 
 # Notify content
 get_content, set_content = stateFactory('Time to take a rest my dear ❤ Drop your glasses, look around.. Is there anything make you interesting?', 'content')
 
 # App mode
-get_mode, set_mode = stateFactory('normal', 'mode')
+get_mode, set_mode = stateFactory('normal', 'hardcore')
 
 # Other config ....
