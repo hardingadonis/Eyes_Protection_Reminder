f418191a39f4d565072bb7fdd4655c835110fcac
Harding Adonis <hardingadonis@gmail.com>
2022-03-21 21:51:15 +0700

    delete old version and add new config

--- src/main.py
+++ src/main.py
@@ -1,18 +1,16 @@
 """
+
 MIT License
 
 Copyright (c) 2022 Harding Adonis, hoanghy0112, AlexPhoenix45
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,12 +21,10 @@ SOFTWARE.
 
 """
 
-import time
-import function.schedule as remind
-from ui.landing import start_landing
-from function.tray import start_tray
+import config as Config
 
 if __name__ == '__main__':
-   remind.start_schedule()
-   start_tray()
-   start_landing()
+    Config.LoadSettings()
+    print(Config.GetConfig("name"))
+    Config.SetConfig("name", "ALOALOALO")
+    pass
