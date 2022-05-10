0af8ab5ddd4d8a74396ff01812faa66aa90dcdc8
Harding Adonis <hardingadonis@gmail.com>
2022-05-10 16:54:03 +0700

    add feature: checking that only a single instance of a program is running 🍳🍳🍳

--- src/application.cpp
+++ src/application.cpp
@@ -4,12 +4,33 @@
 
 bool Application::OnInit()
 {
+	m_checker = new wxSingleInstanceChecker;
+
+	if (m_checker->IsAnotherRunning())
+	{
+		wxLogError(_("Another program instance is already running, aborting."));
+
+		delete m_checker;
+		m_checker = nullptr;
+
+		return false;
+	}
+
+
 	MainFrame* main_frame = new MainFrame("Eyes Protection Reminder - v4.0.0", wxSize(600, 300));
 
 	main_frame->Center();
 	main_frame->Show();
 
 	return true;
+}
+
+int Application::OnExit()
+{
+	delete m_checker;
+	m_checker = nullptr;
+
+	return 0;
 }
 
 wxIMPLEMENT_APP(Application);
--- src/application.hpp
+++ src/application.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
 #include <wx/wx.h>
+#include <wx/snglinst.h>
 
 class Application : public wxApp
 {
 public:
-	bool OnInit();
+	bool OnInit() override;
+
+	int OnExit() override;
+
+private:
+	wxSingleInstanceChecker*	m_checker;
 };
