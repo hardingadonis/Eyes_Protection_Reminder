ab648da28cc98d3d69fffde304460d8c6fad6b73
Harding Adonis <hardingadonis@gmail.com>
2022-09-17 10:01:41 +0700

    first commit for v.4.1.0

--- src/application.cpp
+++ src/application.cpp
@@ -25,7 +25,6 @@
 *********************************************************************************/
 
 #include "application.hpp"
-#include "main_frame.hpp"
 #include <wx/wx.h>
 
 bool Application::OnInit()
@@ -41,12 +40,6 @@ bool Application::OnInit()
 
 		return false;
 	}
-
-
-	MainFrame* main_frame = new MainFrame("Eyes Protection Reminder - v4.0.0", wxSize(600, 300));
-
-	main_frame->Center();
-	main_frame->Show();
 
 	return true;
 }
